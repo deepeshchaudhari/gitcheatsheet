3813cb7bac73764c91cdf85a3fce9916c5191e0f
Deepesh Chaudhari <deepeshchaudhari0786@gmail.com>
2021-01-30 17:51:32 +0530

    ssh
    ssh

--- gitcheatsheet.sh
+++ gitcheatsheet.sh
@@ -1,3 +1,20 @@
+#check which process uses how much memory
+ps -o pid,user,%mem,command ax |sort -b -k3 -r | head -10
+
+# RSA key setup
+# for generating private and public key
+ssh-keygen -t rsa
+#  for copy public key to server
+ssh-copy-id <ip>
+# CHMOD Command
+# 10 char string [owner group anyother]
+# read(r) write(w) execute(x) no permission(-)
+chmod +744
+# command give_permission  owner  group  anyother
+#                           rwx    rwx     rwx
+# chmod         +           111    100     100
+
+
 # Set origin
 git remote set-url origin https://<hostname>/<Username>/<Repository>.git
 
